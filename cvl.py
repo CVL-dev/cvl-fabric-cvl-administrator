7cb890195c91a0c0bd871be7510b99e85acbabfb
James Wettenhall <james.wettenhall@monash.edu>
2012-08-21 05:43:20 +0000

    Added GPL license.

--- cvl.py
+++ cvl.py
@@ -1,14 +1,25 @@
+#  CVL Administrator - A GUI for launching/managing NeCTAR instances.
+#  Copyright (C) 2012  James Wettenhall, Monash University
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#  Enquires: James.Wettenhall@monash.edu or help@massive.org.au
+
 # cvl.py
 """
-A wxPython GUI to provide an easy way to launch a CVL instance, 
-initially on Mac OS X.  It can be run using "python cvl.py",
-assuming that you have a 32-bit version of Python installed,
-wxPython, and the dependent Python modules imported below.
-
-The py2app module is required to build the CVL Administrator.app 
-application bundle, which can be built as follows:
-
-   python create_cvlAdministrator_bundle.py py2app
+A wxPython GUI for the Characterisation Virtual Laboratory
   
 """
 
